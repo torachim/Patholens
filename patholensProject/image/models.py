045bd70b936a778909684bd27f143c09592d58ae
Christoph Mauel <s23cmaue@uni-bonn.de>
2024-11-19 20:12:12 +0100

    Added the diagnosis, useTime models and migrated, registered the image app in settings.py.

--- patholensProject/image/models.py
+++ patholensProject/image/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+# diagnosis class for linkage between the different db entries that "participate" in a certain diagnosis
+class diagnosis(models.Model):
+    diagID = models.AutoField(primary_key = True)
+    # PROTECT: if the referenced user is deleted, the diagnosis won't be deleted
+    userID = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.PROTECT)
+    confidence = models.PositiveSmallIntegerField()
+    subID = models.PositiveIntegerField
+
+    def __str__(self):
+        return self.diagID
+
+# useTime class for storing the timestamps of executed actions during diagnosis
+class useTime(models.Model):
+    timeID = models.AutoField(primary_key = True)
+    # CASCADE: if the referenced diagnosis is deleted, the useTime entry will be automatically deleted aswell
+    diagID = models.ForeignKey(diagnosis, on_delete = models.CASCADE, db_column = "diagID") 
+    action = models.CharField(max_length = 30, null = False)
+    timestamp = models.DurationField(null = False)
+
+    def __str__(self):
+        return self.timeID
