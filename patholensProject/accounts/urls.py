53f42a6d6125ce0a7da8c2bf4f9304697f72d2a2
masterlukeSg <baumeister.luke@web.de>
2024-11-19 14:34:08 +0100

    changed path for login as the function name has changed

--- patholensProject/accounts/urls.py
+++ patholensProject/accounts/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='patholens-login'),
+    path('', views.login_view, name='patholens-login'),
 ]
