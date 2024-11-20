d57cca688faa9b869f7d1bb04b03cf9173e962ed
masterlukeSg <baumeister.luke@web.de>
2024-11-20 18:46:12 +0100

    updatet information dict; userExistent is now accessible; error handling was added, when not using POST

--- patholensProject/accounts/views.py
+++ patholensProject/accounts/views.py
@@ -4,36 +4,49 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from django.shortcuts import redirect
 
+from django.contrib.auth.models import User
 
 def loginView(request):
 
-    information = {"error": False}
+    information = {"username": True,
+                   "password": True,
+                   }
 
-    # when user is already loged in
+    # user is already loged in
     if request.user.is_authenticated:
-
-        print("already logged in")
-        
         # TODO: rediretc to starting page
         return redirect("/")
 
     if request.method == "POST":
-
         email = request.POST.get("email")
         password = request.POST.get("password")
 
-        # remove @ and . so that the usernmae is email without the special characters
+        # remove '@' and '.' so that the usernmae is the email but without the special characters
         email = email.replace("@", "")
         email = email.replace(".", "")
 
+        userExistent = User.objects.filter(username=email).exists()
 
+        
+        # user is not existent 
+        if (userExistent == False):
+            information["username"] = False
+            return render(request, "accounts/login.html", information)
+        
+        
         user = authenticate(username=email, password=password)
 
+        # login was successful
         if user is not None:
             login(request, user)
             return redirect("/")
         
+        # password is incorrect
         else:
-            information["error"] = True
-
+            information["password"] = False
+    
+    else:
+        print("Error as POST was not used")
+        
+        
     return render(request, "accounts/login.html", information)
