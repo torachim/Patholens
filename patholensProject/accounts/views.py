d6cdd8101f0b219a064353b6614fa431cbb4d9c0
masterlukeSg <baumeister.luke@web.de>
2024-11-20 14:39:06 +0100

    username is again used as authentication. However, our username is the email

--- patholensProject/accounts/views.py
+++ patholensProject/accounts/views.py
@@ -22,7 +22,8 @@ def login_view(request):
         email = request.POST.get("email")
         password = request.POST.get("password")
         
-        user = authenticate(email=email, password=password)
+
+        user = authenticate(username=email, password=password)
         
         if (user is not None):
             login(request, user)
