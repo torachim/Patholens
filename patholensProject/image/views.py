38e0f1e2ea9a49458cc4957f32d42be4400df350
imenebenamer <s14ibena@uni-bonn.de>
2024-11-28 17:26:24 +0100

    added url for the diagnosis page

--- patholensProject/image/views.py
+++ patholensProject/image/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from django.shortcuts import redirect
 
 # Create your views here.
+
+def diagnosisView(request):
+    return render(request, 'diagnosisPage.html')
