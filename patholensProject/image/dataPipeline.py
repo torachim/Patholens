65e93a0e40ebf3550d3bea051a53da179395a0f9
masterlukeSg <baumeister.luke@web.de>
2024-11-30 15:57:23 +0100

    path to the data Set is now independent from the execution path

--- patholensProject/image/dataPipeline.py
+++ patholensProject/image/dataPipeline.py
@@ -1,21 +1,21 @@
 import os
+from pathlib import Path
+
+BASEDIR = Path(__file__).resolve().parent.parent
+DATASETPATH = os.path.join(BASEDIR, "dataSets")
 
 
 # go through all data Sets in the directory and return the names of the data Sets
 def getDataSets():
+    global BASEDIR, DATASETPATH
 
     allDataSets = []
 
-    pathExists = os.path.exists("dataSets")
-    pathIsDirectory = os.path.isdir("dataSets")
+    pathExists = os.path.exists(DATASETPATH)
+    pathIsDirectory = os.path.isdir(DATASETPATH)
 
     if pathExists and pathIsDirectory:
-        for dir in os.listdir("dataSets"):
+        for dir in os.listdir(DATASETPATH):
             allDataSets.append(dir)
 
-    print(allDataSets)
     return allDataSets
-
-
-if __name__ == "__main__":
-    getDataSets()
