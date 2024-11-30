02d46876cf94d074d5f41b37c6ba4f628918ad75
imenebenamer <s14ibena@uni-bonn.de>
2024-11-30 21:46:57 +0100

    added the t1 and flaire select buttons

--- patholensProject/image/urls.py
+++ patholensProject/image/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    
     path('', include('accounts.urls')),
     path('diagnosis/', views.diagnosisView, name='patholensDiagnosis'),
 ]
+
