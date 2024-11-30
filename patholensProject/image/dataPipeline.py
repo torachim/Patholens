253915020c4a18c840b4666623fa1e27557a9295
masterlukeSg <baumeister.luke@web.de>
2024-11-30 18:58:46 +0100

    addAllPatientsToDoctorsDB gets all ids from patients and saves it in a dict/JSON format

--- patholensProject/image/dataPipeline.py
+++ patholensProject/image/dataPipeline.py
@@ -23,14 +23,26 @@ def getDataSets():
 
 def addAllPatientsToDoctorsDB(docID):
     global DATASETPATH
-    
+
     remainingPatients = {}
 
     allDataSets = getDataSets()
-    
+
     for dataSet in allDataSets:
-        print(dataSet)
-        
+        # has all the paths to the availabe data sets
+        allSubPaths = os.listdir(os.path.join(DATASETPATH, dataSet))
+
+        allSubIDs = []
+        for sub in allSubPaths:
+            if "sub-" in sub:
+                # splits the string at '-' and takes only the number of the string (our ID)
+                subID = sub.split("-")[1]
+                allSubIDs.append(subID)
+
+        remainingPatients[dataSet] = {"url": allSubIDs}
+
+    return True
+
 
 if "__main__" == __name__:
     addAllPatientsToDoctorsDB(2)
