b361a43db3a5f89883920c818ab0328985d4da1b
Imad <s47iaziz@uni-bonn.de>
2024-12-01 12:58:07 +0100

    implemented an alert to js file to see if it connects right with html and css

---
+++ patholensProject/image/static/js/index.js
@@ -0,0 +1,5 @@
+// index.js in static/js/
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Die Seite wurde vollständig geladen.');
+    alert('Test Test - JavaScript ist aktiv!');
+});
