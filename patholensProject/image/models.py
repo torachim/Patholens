76371db62c56dfebd520d1248e33b77a95bc3af1
masterlukeSg <baumeister.luke@web.de>
2024-12-02 20:52:53 +0100

    class name beginns now with upper case

--- patholensProject/image/models.py
+++ patholensProject/image/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 from django.conf import settings
 
+from pathlib import Path
+import sys
+import os
+import django
+
+#sys.path.append(str(Path(__file__).resolve().parent.parent))
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "patholensProject.settings")
+
+#if not settings.configured:  
+#    django.setup()
+
+from accounts.models import Doctors
+
 # Create your models here.
 
 
 # diagnosis class for linkage between the different db entries that "participate" in a certain diagnosis
-class diagnosis(models.Model):
+class Diagnosis(models.Model):
     diagID = models.AutoField(primary_key = True)
     # PROTECT: if the referenced user is deleted, the diagnosis won't be deleted
     userID = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.PROTECT)
@@ -16,10 +29,10 @@ class diagnosis(models.Model):
         return str(self.diagID)
 
 # useTime class for storing the timestamps of executed actions during diagnosis
-class useTime(models.Model):
+class UseTime(models.Model):
     timeID = models.AutoField(primary_key = True)
     # CASCADE: if the referenced diagnosis is deleted, the useTime entry will be automatically deleted aswell
-    diagID = models.ForeignKey(diagnosis, on_delete = models.CASCADE, db_column = "diagID") 
+    diagID = models.ForeignKey(Diagnosis, on_delete = models.CASCADE, db_column = "diagID") 
     action = models.CharField(max_length = 30, null = False)
     timestamp = models.DurationField(null = False)
 
--- patholensProject/image/tests.py
+++ patholensProject/image/tests.py
@@ -1,12 +1,29 @@
 from django.test import TestCase
 from dataPipeline import *
 import unittest
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 
 class TestDataPipeline(unittest.TestCase):
+
     def testGetDataSets(self):
         rightOutput = ["websiteData"]
         self.assertEqual(getDataSets(), rightOutput)
+
+    def setUp(self):
+        # Create a test user
+        self.user = User.objects.create_user(
+            username="whatATestATgmailPOINTcom",
+            password="password",
+            email="whatATest@gmail.com",
+        )
+
+        # Log the user in
+        self.client.login(username="whatATestATgmailPOINTcom", password="password")
+
+        # Store the user's ID for later use
+        self.user_id = self.user.id
 
     def testAddAllPatientsToDoctorsDB(self):
         rightOutput = {"websiteData": {"url": ["00001", "00123"]}}
