eb279ddc4d92c7289c4b84cc996af04e7624b452
Imad <s47iaziz@uni-bonn.de>
2024-12-02 11:18:30 +0100

    added the url for NIfTI image to load image on browser

--- patholensProject/image/static/js/main.js
+++ patholensProject/image/static/js/main.js
@@ -4,4 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
   const nv = new Niivue();
   const canvas = document.getElementById('niivue-canvas');
   nv.attachToCanvas(canvas);
+  const flairUrl = '/static/images/sub-00003_space-orig_FLAIR.nii.gz';
+  nv.loadVolumes([{ url: flairUrl }]);
 });
