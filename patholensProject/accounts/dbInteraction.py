8356a5ecc703a030cbab7cc87bc5cb10279eb279
masterlukeSg <baumeister.luke@web.de>
2024-12-03 00:20:46 +0100

    generateIdsForUrls makes a JSON for doctor Db allPatients (key=id, value=imageUrl

--- patholensProject/accounts/dbInteraction.py
+++ patholensProject/accounts/dbInteraction.py
@@ -1,7 +1,46 @@
-from accounts.models import Doctors
 import uuid
+import os
+import sys
+import django
+from pathlib import Path
+
+# Add project path (root directory where manage.py is located)
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+# Define Django settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "patholensProject.settings")
+
+# Initialize Django
+django.setup()
 
 
+import image.dataPipeline as dataPipeline
+#from models import Doctors
+
+
+
+
+def generateIdsForUrls(allDataSets, allUrls):
+
+    ids = {}
+
+    for dataSet in allDataSets:
+        urls = allUrls[dataSet]["url"]
+        amount = len(urls)
+        uuid = createUUIDs(amount)
+
+        # Initialize the dictionary for dataSet if it does not exist yet
+        if dataSet not in ids:
+            ids[dataSet] = {}
+
+        # Add urls and uuids to dictionary
+        for index in range(amount):
+            ids[dataSet][uuid[index]] = urls[index]
+
+    return ids
+
+
+# creates random ids
 def createUUIDs(amount):
 
     allUUIDs = []
@@ -9,3 +48,6 @@ def createUUIDs(amount):
         allUUIDs.append(str(uuid.uuid4()))
 
     return allUUIDs
+
+if __name__ == "__main__":
+    createDoctor()
