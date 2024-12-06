67e109f53a70f7207173182039d73ef9585f6422
snehpreet <skaurd1234@gmail.com>
2024-12-06 13:10:26 +0100

    designed the select datset container

--- patholensProject/patholensProject/views.py
+++ patholensProject/patholensProject/views.py
@@ -9,14 +9,10 @@ def homeWindow(request):
     return render(request, 'homeWindow.html')
 
 def data(request):
-    return render(request, 'selectDataset.html')
-
-def datasets_view(request):
     allDataSets = [
-        "Climate Change Data 2023",
-        "World Population Statistics 2024",
-        "COVID-19 Research Dataset",
-        "Global Financial Markets Data",
-        "ImageNet 2024: Computer Vision Dataset",
+        "Dataset 1",
+        "Dataset 2",
+        "Dataset 3",
     ]
-    return render(request, 'datasets.html', {'allDataSets': allDataSets})
+     
+    return render(request, 'selectDataset.html', {'allDataSets': allDataSets})
