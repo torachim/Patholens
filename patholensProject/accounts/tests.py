ceedbc1f9cf6db2cfac3bfa40534b3ceda390e5a
masterlukeSg <baumeister.luke@web.de>
2024-12-09 22:07:38 +0100

    get Picture function was test such as getDocotrObject and 'get Random Id and Url' function

--- patholensProject/accounts/tests.py
+++ patholensProject/accounts/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from doctorManager import *
+from diagnosisManager import *
 import unittest
 from accounts.models import Doctors
 from image.models import Diagnosis
@@ -40,12 +41,40 @@ class TestDoctorsManager(unittest.TestCase):
 
     def testRandomPicture(self):
         idFromTestUser = self.doc.doctorID
-        print(getRandomDiagnosis(idFromTestUser, "websiteData"))
-
+        print(getRandomIdAndUrl(idFromTestUser, "websiteData"))
+        
+    
     # is called last
     def tearDown(self):
         self.testUser.delete()
 
+class TestDiagnosisManager(unittest.TestCase):
+    # is called first
+    def setUp(self):
+        # Create a test user
+        self.testUser = User.objects.create_user(
+            username="lukaZWEIsTestffeATgmailPOINTcom",
+            email="lukasZWEITesteff@gmail.com",
+            first_name="Test",
+            last_name="User2",
+            password="kleinesAberFeines7503",
+        )
+        self.doc = createDoctor(self.testUser)
 
+        
+        
+    def testGetPicture(self):
+        idDiag, urlForPicture = getRandomIdAndUrl(self.doc.doctorID, 'website_data')
+        docObject = getDoctorObject(self.testUser.id)
+        
+        diag = createDiagnosis(idDiag, docObject, urlForPicture)
+        
+        self.assertEqual(getUrl(idDiag), diag.imageUrl)
+     
+    # is called last
+    def tearDown(self):
+        self.testUser.delete()
+    
+    
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(defaultTest='TestDiagnosisManager')
