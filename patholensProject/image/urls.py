23678a5a549e0c2144245727055bc90be0dc0621
masterlukeSg <baumeister.luke@web.de>
2024-12-09 22:40:04 +0100

    changed apiViews.py to CamalCase

--- patholensProject/image/urls.py
+++ patholensProject/image/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.urls import path, include
 from . import views
-from .api_views import GetImageAPIView
+from .apiViews import GetImageAPIView
 
 urlpatterns = [
     
