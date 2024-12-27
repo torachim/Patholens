e59aa57605916d51b7e1c245be33d1d690bbc472
Imad <s47iaziz@uni-bonn.de>
2024-12-27 11:07:28 +0100

    added the getCookie function to call CSRF Token

--- patholensProject/image/static/js/main.js
+++ patholensProject/image/static/js/main.js
@@ -250,6 +250,22 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     
     
+    // CSRF-Token abrufen
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== "") {
+            const cookies = document.cookie.split(";");
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                if (cookie.substring(0, name.length + 1) === `${name}=`) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    
     // Confirm-Button-Listener
     document.getElementById("confirmButton").addEventListener("click", saveEditedImage);
     
