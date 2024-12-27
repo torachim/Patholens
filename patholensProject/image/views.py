b3d7226710abcde1a37d21aba7a720d94415c44a
Imad <s47iaziz@uni-bonn.de>
2024-12-27 11:10:04 +0100

    implemented the save_edited_image function in views.py to enable POST request to server to save Image in media Folder

--- patholensProject/image/views.py
+++ patholensProject/image/views.py
@@ -19,3 +19,28 @@ def newDiagnosis(request, diagnosisID):
 
 def testRenderImageView(request, imageID):
     return render(request, 'image/diagnosisPage.html', {'imageID': imageID})
+
+
+def save_edited_image(request):
+    if request.method == "POST":
+        try:
+            # Datei und Dateiname aus der Anfrage extrahieren
+            image_file = request.FILES.get("image_file")
+            filename = request.POST.get("filename")
+
+            if not image_file or not filename:
+                return JsonResponse({"error": "Invalid data"}, status=400)
+
+            # Speicherpfad im Media-Ordner
+            filepath = os.path.join(settings.MEDIA_ROOT, filename)
+
+            # Speichere die Datei
+            with open(filepath, "wb") as f:
+                for chunk in image_file.chunks():
+                    f.write(chunk)
+
+            return JsonResponse({"message": "Image saved successfully", "path": filepath})
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=400)
+    else:
+        return JsonResponse({"error": "Invalid request method"}, status=405)
