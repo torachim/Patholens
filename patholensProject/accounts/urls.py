90122b3a0cfaea11e7e8607e576ef9c1c9fd4437
Imad <s47iaziz@uni-bonn.de>
2024-12-28 23:16:11 +0100

    added path url for getURL view

--- patholensProject/accounts/urls.py
+++ patholensProject/accounts/urls.py
@@ -21,5 +21,8 @@ from . import views
 urlpatterns = [
     path('', views.loginView, name='patholensLogin'),
     path('signup/', views.signupView, name='patholensSignUp'),
-    path('logout/<str:calledFrom>/', views.logoutView, name = 'patholensLogout')
+    path('logout/<str:calledFrom>/', views.logoutView, name = 'patholensLogout'),
+
+    path('getURL/<str:diagID>/', views.getURLApi, name='getUrlApi'),
+
 ]
