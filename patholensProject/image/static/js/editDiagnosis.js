61bb1d81c3a8f6a6db46bdfeaf73544fed3bfa4a
Christoph Mauel <s23cmaue@uni-bonn.de>
2025-01-03 20:23:34 +0100

    started implementing the zoom button

---
+++ patholensProject/image/static/js/editDiagnosis.js
@@ -0,0 +1,21 @@
+import Niivue from "./index.js";
+
+// Loading the images
+
+const compareNv = new Niivue();
+const compareCanvas = document.getElementById("comparisonContainer");
+
+// Load FLAIR default
+let selectedFormat = "FLAIR";
+
+
+
+
+// Zoom
+const comparisonContainer = document.getElementById("comparisonContainer");
+document.getElementById("zoomButton").addEventListener("click", () =>{
+    comparisonContainer.style.left = "20%";
+    comparisonContainer.style.width = "60%";
+    comparisonContainer.style.top = "35%";
+    comparisonContainer.style.height = "50%";
+})
