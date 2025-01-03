45ddd7de8cf95708a1233832033a94ab1f09a14e
Christoph Mauel <s23cmaue@uni-bonn.de>
2025-01-03 10:31:52 +0100

    fixed the issue that the confidence meter was visible while loading the diagnosisPage + added some comments to html files + removed some unnecessary lines

--- patholensProject/image/static/js/main.js
+++ patholensProject/image/static/js/main.js
@@ -329,6 +329,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const finishButton = document.getElementById("finishButton");
     const popupOverlay = document.getElementById("popupOverlay");
     const closePopup = document.getElementById("closePopup");
+    const popupFrame = document.getElementById("popupFrame");
 
     popupOverlay.style.display = "none";
 
@@ -336,6 +337,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Function to show the confidence window
     finishButton.addEventListener("click", () => {
         popupOverlay.style.display = "flex";
+        popupFrame.style.display = "block";
         startTimer();
     });
 
