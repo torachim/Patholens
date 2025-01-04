deb6d62a76e11688d09cccd1a0e689ef2e52a4a0
Christoph Mauel <s23cmaue@uni-bonn.de>
2025-01-04 14:11:11 +0100

    adjusted the css and html trying to make the website fit different devices

--- patholensProject/image/static/js/editDiagnosis.js
+++ patholensProject/image/static/js/editDiagnosis.js
@@ -1,21 +1,25 @@
 import Niivue from "./index.js";
 
-// Loading the images
+document.addEventListener('DOMContentLoaded', function() {
+    // Loading the images
 
-const compareNv = new Niivue();
-const compareCanvas = document.getElementById("comparisonContainer");
+    const compareNv = new Niivue();
+    const compareCanvas = document.getElementById("comparisonContainer");
 
-// Load FLAIR default
-let selectedFormat = "FLAIR";
-
-
+    // Load FLAIR default
+    let selectedFormat = "FLAIR";
 
 
-// Zoom
-const comparisonContainer = document.getElementById("comparisonContainer");
-document.getElementById("zoomButton").addEventListener("click", () =>{
-    comparisonContainer.style.left = "20%";
-    comparisonContainer.style.width = "60%";
-    comparisonContainer.style.top = "35%";
-    comparisonContainer.style.height = "50%";
-})
+
+
+    // Zoom
+
+    const comparisonContainer = document.getElementById("comparisonContainer");
+    document.getElementById("zoomButton").addEventListener("click", () =>{
+        comparisonContainer.style.position ="absolute";
+        comparisonContainer.style.left = "20%";
+        comparisonContainer.style.width = "60%";
+        comparisonContainer.style.top = "35%";
+        comparisonContainer.style.height = "50%";
+    });
+});
