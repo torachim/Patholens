462336a1eacce62e3ac4efda4e1041debca7e2db
Christoph Mauel <s23cmaue@uni-bonn.de>
2025-01-04 16:11:25 +0100

    added zoom out functionality by clicking on the zoom button again. Also added that the plus in the button changes to a minus when zoomed

--- patholensProject/image/static/js/editDiagnosis.js
+++ patholensProject/image/static/js/editDiagnosis.js
@@ -16,10 +16,24 @@ document.addEventListener('DOMContentLoaded', function() {
     // Zoom
 
     const comparisonContainer = document.getElementById("comparisonContainer");
-    document.getElementById("zoomButton").addEventListener("click", () =>{
-        console.log("button pressed")
-        comparisonContainer.style.width = "74%";
-        comparisonContainer.style.top = "35%";
-        comparisonContainer.style.height = "53%";
+    const zoomButton = document.getElementById("zoomButton");
+    let zoomed = false;
+    zoomButton.addEventListener("click", () =>{
+        if(zoomed){
+            comparisonContainer.style.width = "50%";
+            comparisonContainer.style.top = "";
+            comparisonContainer.style.height = "24%";
+            zoomButton.src = "/static/icons/editPageZoomButton.png";
+            zoomed = false;
+        }
+
+        else{
+            comparisonContainer.style.width = "74%";
+            comparisonContainer.style.top = "35%";
+            comparisonContainer.style.height = "53%";
+            zoomButton.src = "/static/icons/editPageZoomOutButton.png";
+            zoomed = true;
+        }
+
     });
 });
