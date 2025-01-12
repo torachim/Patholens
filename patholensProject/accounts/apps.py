45eaf5d8eb69a6c59e3dc40f04d977b51f2e050d
Lukas Baumeister <baumeister.luke@web.de>
2025-01-12 11:47:40 +0100

    All datasets are added to the Doctor object when it is created.

--- patholensProject/accounts/apps.py
+++ patholensProject/accounts/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+    
+    def ready(self):
+        # Hier registrieren wir das Signal
+        import accounts.signals
