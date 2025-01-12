33bdd270170c2e3c536a9181ba2924d70627fed3
Lukas Baumeister <baumeister.luke@web.de>
2025-01-12 12:10:28 +0100

    Datasets can be diabled from Doctor (with the admin panel)

--- patholensProject/accounts/admin.py
+++ patholensProject/accounts/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from .models import Doctors
-
+from django import forms
+from image.models import Media
 # Register your models here.
 
-admin.site.register(Doctors)
+
+class DoctorForm(forms.ModelForm):
+    class Meta:
+        model = Doctors
+        # show every field in the panel
+        fields = '__all__'
+        widgets = {
+            'datasets': forms.CheckboxSelectMultiple,
+        }
+
+
+class DoctorAdmin(admin.ModelAdmin):
+    form = DoctorForm
+    filter_horizontal = ('datasets',)
+
+admin.site.register(Doctors, DoctorAdmin)
