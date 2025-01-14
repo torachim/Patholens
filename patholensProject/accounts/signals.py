5fcb05d9aa4476ad06a77f1eac787a290a26e4c3
Lukas Baumeister <baumeister.luke@web.de>
2025-01-14 16:12:10 +0100

    call addMedia befor creating a doctor. Fixed the bug as now the medias are in the DB.

--- patholensProject/accounts/signals.py
+++ patholensProject/accounts/signals.py
@@ -1,16 +1,20 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core.cache import cache
 
 from .models import Doctors
 from image.models import Media
+from image.mediaHandler import addMedia
+
 
 # Always triggered when a doctor is saved
 @receiver(post_save, sender=Doctors)
 def assign_default_datasets(sender, instance, created, **kwargs):
     # Only when a doctor is created the code gets triggered
     if created:
+        addMedia()
         # All existing Media from the database
-        all_datasets = Media.objects.all()
+        allDatasets = Media.objects.all()
         # Add the datasets to the Doctor
-        instance.datasets.set(all_datasets) 
+        instance.datasets.set(allDatasets)
         
