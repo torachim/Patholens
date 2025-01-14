c1373ae1d2caa58f08447889626599e55c324dfe
Imad <s47iaziz@uni-bonn.de>
2025-01-14 10:30:37 +0100

    moved the api's in views to apiViews

--- patholensProject/accounts/urls.py
+++ patholensProject/accounts/urls.py
@@ -17,13 +17,15 @@ Including another URLconf
 
 from django.urls import path
 from . import views
+from .apiViews import getURLApi
+from .apiViews import getDocID
 
 urlpatterns = [
     path('', views.loginView, name='patholensLogin'),
     path('signup/', views.signupView, name='patholensSignUp'),
     path('logout/<str:calledFrom>/', views.logoutView, name = 'patholensLogout'),
 
-    path('api/getURL/<str:diagID>/', views.getURLApi, name='getUrlApi'),
-    path('api/getDoctorID/', views.getDocID, name='getDocID'),
+    path('api/getURL/<str:diagID>/', getURLApi, name='getUrlApi'),
+    path('api/getDoctorID/', getDocID, name='getDocID'),
 
 ]
