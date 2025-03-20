c2f470f2e66472910204f44d91b395074f74212b
Lukas Baumeister <baumeister.luke@web.de>
2025-03-20 17:59:05 +0100

    AI model entries are now getting synced.

--- patholensProject/image/aiModelServices.py
+++ patholensProject/image/aiModelServices.py
@@ -1,6 +1,33 @@
-from image.dataHandler import *
-from .models import Media
+from image.dataHandler import getAIModelNamesFromMediaFolder
+from .models import AiModel, Media
 
+def syncAIEntries(dataset):
+    """
+    Retrieves all the possible ai model names of a dataset and adds them to the database.
 
-def sync():
-    pass
+    Args:
+        dataset (str): The name of the dataset.
+
+    Returns:
+        bool: True when everythin works
+    """
+    
+    aiModels: list[str.upper] = getAIModelNamesFromMediaFolder(dataset)
+    
+    for model in aiModels:
+        # Skip, if the objects exists
+        if AiModel.objects.filter(modelName=model).exists():
+            continue
+        # Skip, if the Media entry is not existing any more
+        if not Media.objects.filter(name=dataset).exists():
+            continue
+        
+        # Create a new ai model entry
+        media: Media = Media.objects.get(name=dataset) 
+        AiModel.objects.create(modelName=model, mediaEntry=media, visibility=True)
+    
+    return True
+            
+                 
+    
+    
